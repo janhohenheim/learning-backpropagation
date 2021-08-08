f1d314f89a9258075f28a18eeb0cbf224bf424a0
Jan Nils Ferner <contact@jnferner.com>
2021-08-08 18:28:42 +0200

    Play around with matrices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use nalgebra::{Matrix2, SMatrix, Vector2};
+use rand::Rng;
+use std::f32::consts::E;
+use std::ops::Range;
+
+fn sigmoid(n: f32) -> f32 {
+    1.0 / (1.0 + E.powf(n))
+}
+
+fn generate_number(range: Range<f32>) -> f32 {
+    rand::thread_rng().gen_range(range)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let big_matrix = SMatrix::<f32, 4, 4>::from_fn(|_i, _j| generate_number(0.0..1.0));
+    println!("{}", big_matrix);
+    let sigmoid_matrix = big_matrix.map(sigmoid);
+    println!("{}", sigmoid_matrix);
+
+    let linear_transformation_matrix = Matrix2::new(1, 3, 2, 1);
+    let original_vector = Vector2::new(4, 2);
+    let transformed_vector = linear_transformation_matrix * original_vector;
+    println!("{}", transformed_vector);
+
+    let ninety_degree_spin = Matrix2::new(0, -1, 1, 0);
+    let shear = Matrix2::new(1, 2, 0, 1);
+    let combined_matrix = ninety_degree_spin * shear;
+    println!("{}", combined_matrix);
 }
