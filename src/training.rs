7863be941107f0fbd53d67d73b05b56192dcd1d3
Jan Nils Ferner <contact@jnferner.com>
2021-08-23 23:23:08 +0200

    Refactor code

--- src/training.rs
+++ src/training.rs
@@ -4,29 +4,37 @@ use crate::gradient_descent::gradient_descent;
 use crate::neural_network::{get_activations, Parameters};
 use crate::training_data::TrainingData;
 
+fn generate_empty_gradients(parameters: &Parameters) -> Vec<Gradients> {
+    parameters
+        .weights
+        .iter()
+        .zip(parameters.biases.iter())
+        .map(|(weights, biases)| Gradients::zeros_like(weights, biases))
+        .collect::<Vec<_>>()
+}
+
+fn add_gradients(mut gradient_sum: Vec<Gradients>, summand: Vec<Gradients>) -> Vec<Gradients> {
+    gradient_sum
+        .iter_mut()
+        .zip(summand.into_iter())
+        .for_each(|(gradient_sum, current_gradient)| *gradient_sum += current_gradient);
+    gradient_sum
+}
+
 pub fn train(
     training_data: &[TrainingData],
     mut parameters: &mut Parameters,
     learning_configuration: &LearningConfiguration,
 ) {
-    let empty_gradients = parameters
-        .weights
-        .iter()
-        .zip(parameters.biases.iter())
-        .map(|(weights, biases)| Gradients::zeros_like(weights, biases))
-        .collect::<Vec<_>>();
+    let empty_gradients = generate_empty_gradients(parameters);
     let gradients = training_data
         .iter()
         .map(|training_data| {
             let activations = get_activations(&training_data.inputs, parameters);
             backpropagate(&parameters.weights, &activations, &training_data.labels)
         })
-        .fold(empty_gradients, |mut all_gradients, current_gradients| {
-            all_gradients
-                .iter_mut()
-                .zip(current_gradients.into_iter())
-                .for_each(|(total_gradient, current_gradient)| *total_gradient += current_gradient);
-            all_gradients
+        .fold(empty_gradients, |gradient_sum, current_gradients| {
+            add_gradients(gradient_sum, current_gradients)
         });
     gradient_descent(&mut parameters, &gradients, learning_configuration);
 }
